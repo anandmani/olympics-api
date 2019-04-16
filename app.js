ad1f12bdf5d3878020f9f9ff6750dbcd65a8cbe3
anandmani <cma.camelot@gmail.com>
2019-04-16 13:00:04 -0400

    Added nodemon; Set response type to JSON

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next) => {
+  res.type('json');
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
