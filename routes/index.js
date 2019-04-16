5ed0fbc432f2263f5de29d2831ac4fbf8f2d5f79
anandmani <cma.camelot@gmail.com>
2019-04-16 13:12:21 -0400

    Added trend1 without filters

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,22 @@ router.get('/test', async function(req, res, next) {
   res.send(result);
 });
 
+router.get('/trend1', async function(req, res, next) {
+  let id = req.query.id;
+  let db_result = await database.simpleExecute(`
+    select count(*) as count, game.year 
+    from athlete, participant, competes_in,game 
+    where athlete.sex='F' AND 
+      athlete.id=participant.athlete_id AND 
+      participant.id=competes_in.participant_id AND
+      competes_in.game_id=game.id AND 
+      game.season='Summer' 
+    group by game.year 
+    order by game.year
+  `);
+  let result = JSON.stringify(db_result.rows)
+  console.log(result);
+  res.send(result);
+});
+
 module.exports = router;
