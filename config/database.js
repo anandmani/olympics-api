670b9a4ac1aee8b34232b8e281f669c28f3152f4
anandmani <cma.camelot@gmail.com>
2019-04-16 12:54:48 -0400

    Added test route

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   hrPool: {
-    user: "mani",
-    password: "test1234",
+    user: "aalsalmi",
+    password: "AhAs708303",
     connectString: "oracle.cise.ufl.edu/orcl",
     poolMin: 10,
     poolMax: 10,
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,18 @@ var router = express.Router();
 const database = require('../services/database.js');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-
+router.get('/test', async function(req, res, next) {
       // let result = await connection.execute(
     //   `SELECT manager_id, department_id, department_name
     //    FROM departments
     //    WHERE manager_id = :id`,
     //   [103],  // bind value for :id
     // );
-    
-  const result = await database.simpleExecute('SELECT * FROM ATHLETE WHERE ID = 6157');
-  console.log(result.rows);
-  res.render('index', { title: 'Express' });
+  let id = req.query.id;
+  let db_result = await database.simpleExecute(`SELECT * FROM ATHLETE WHERE ID = ${id}`);
+  let result = JSON.stringify(db_result.rows)
+  console.log(result);
+  res.send(result);
 });
 
 module.exports = router;
